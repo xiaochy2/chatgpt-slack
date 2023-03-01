16548fa8e7980142df9d8bef6f22a3e38258826d
Tim Yang <xiaochy2@uci.edu>
2023-03-01 17:19:19 -0600

    debug

--- src/app.ts
+++ src/app.ts
@@ -18,26 +18,27 @@ app.message("hello", async ({ message, say }) => {
   // Filter out message events with subtypes (see https://api.slack.com/events/message)
   if (message.subtype === undefined || message.subtype === "bot_message") {
     // say() sends a message to the channel where the event was triggered
-    await say({
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: `Hey there <@${message.user}>!`,
-          },
-          accessory: {
-            type: "button",
-            text: {
-              type: "plain_text",
-              text: "Click Me",
-            },
-            action_id: "button_click",
-          },
-        },
-      ],
-      text: `Hey there <@${message.user}>!`,
-    });
+    say(`Hey there <@${message.user}>!`);
+    // await say({
+    //   blocks: [
+    //     {
+    //       type: "section",
+    //       text: {
+    //         type: "mrkdwn",
+    //         text: `Hey there <@${message.user}>!`,
+    //       },
+    //       accessory: {
+    //         type: "button",
+    //         text: {
+    //           type: "plain_text",
+    //           text: "Click Me",
+    //         },
+    //         action_id: "button_click",
+    //       },
+    //     },
+    //   ],
+    //   text: `Hey there <@${message.user}>!`,
+    // });
   }
 });
 
